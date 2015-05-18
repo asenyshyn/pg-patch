8e6ef5ae22c7b0f5a9d50cf2258368d6a7bc8e1f
Andriy Senyshyn <sam@maytech.net>
2015-05-18 20:00:45 +0300

    updated graph tool. changed .txt to .md

--- tools/make_patch_graph.sh
+++ tools/make_patch_graph.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "digraph { size=\"16,12\";" > /C/tmp/file.dot
-tools/list-dependencies-from-patches.sh patches/*.sql |sed 's/ / -> /g' |sed 's/$/;/' >> /C/tmp/file.dot
-echo "}" >> /tmp/file.dot
-#dot -Nshape=box  /tmp/file.dot > /tmp/file.jpeg
+tools/list-dependencies-from-patches.sh patches/*.sql |
+    sed 's/^/"/g' |
+    sed 's/ /" -> "/g' |
+    sed 's/$/";/g' >> /C/tmp/file.dot
+echo "}" >> /C/tmp/file.dot
+#dot -Nshape=box  /tmp/file.dot > /C/tmp/file.jpeg
